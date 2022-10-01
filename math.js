f59a1a2f3dadb7f86c6205dcc4736054ee4ed34f
KawserAhmed0321 <kanonmmc@gmail.com>
2022-10-01 11:16:32 +0600

    swap

---
+++ math.js
@@ -0,0 +1,21 @@
+//const myNumber=-5;
+//const output = Math.abs(myNumber);
+
+//const myNumber=21.99;
+//const output =Math.ceil(myNumber)
+
+const myNumber = 21.99;
+const output =Math.floor(myNumber)
+console.log(output);
+
+//swap variable
+var first=5;
+var second=7;
+ console.log(first,second);
+// var temp=first;
+// first=second;
+// second=temp;
+// console.log(first,second);
+[first,second]=[second,first]
+console.log(first, second);
+ 
